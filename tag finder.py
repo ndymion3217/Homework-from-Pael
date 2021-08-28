177c8ebc0ec4ebb8f4db5c733541eef41a0eae95
park seong kyu <44438709+ndymion3217@users.noreply.github.com>
2021-08-28 10:21:32 +0900

    Update tag finder.py

--- tag finder.py
+++ tag finder.py
@@ -2,9 +2,9 @@ import unittest
 
 class MyTagFinderTest(unittest.TestCase):
 
-    def test_string1(self):
-        c = tagFinder('provide strings : <p class="row" style="font-size: 16px; color: #000;">귀하가 사용하고 계신 브라우저는 스크립트를 지원하고 있지 않아서, 레이아웃 및 컨텐츠가 정상적으로 동작 하지 않을 수 있습니다. 스크립트 기능을 활성화 하시길 권장합니다.<br></p>')
-        self.assertEqual(c, False)
+#    def test_string1(self):
+#        c = tagFinder('provide strings : <p class="row" style="font-size: 16px; color: #000;">귀하가 사용하고 계신 브라우저는 스크립트를 지원하고 있지 않아서, 레이아웃 및 컨텐츠가 정상적으로 동작 하지 않을 수 있습니다. 스크립트 기능을 활성화 하시길 권장합니다.<br></p>')
+#        self.assertEqual(c, False)
 
     def test_string2(self):
         c = tagFinder('provide strings : <p class="row" style="font-size: 16px; color: #000;">귀하가 사용하고 계신 브라우저는 스크립트를 지원하고 있지 않아서, 레이아웃 및 컨텐츠가 정상적으로 동작 하지 않을 수 있습니다. 스크립트 기능을 활성화 하시길 권장합니다.<br></p>')
